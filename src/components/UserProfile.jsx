2fe6b1b706fa73c569a1e07ead67518f3755eb3c
jtx007 <jamesjacobthomas7@gmail.com>
2020-02-12 19:24:21 -0500

    fixed displayName update on userProfile

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,14 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { firestore, auth } from "../firebase";
 
 class UserProfile extends Component {
-    render() {
-        return (
-            <div>
-                User Profile
-            </div>
-        )
+  state = { displayName: "" };
+  imageInput = null;
+
+ 
+
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { displayName } = this.state;
+    if (displayName) {
+      firestore.doc(`users/${auth.currentUser.uid}`).update({ displayName });
     }
+  };
+
+  render() {
+    const { displayName } = this.state;
+
+    return (
+      <section className="UserProfile">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            value={displayName}
+            onChange={this.handleChange}
+            type="text"
+            name="displayName"
+          />
+          <input type="file" ref={ref => this.imageInput = ref} />
+          <input className="update"  type="submit" />
+        </form>
+      </section>
+    );
+  }
 }
 
-
-export default UserProfile
+export default UserProfile;
